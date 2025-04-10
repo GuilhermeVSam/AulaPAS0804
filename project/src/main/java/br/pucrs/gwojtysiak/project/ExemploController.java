d0802e83cf7cab780c8b27d43d0bc9a42d06ef24
Guilherme Velho Wojtysiak <guiwojtysiak053@hotmail.com>
2025-04-10 23:17:07 +0000

    QueryString e PathVariable

--- project/src/main/java/br/pucrs/gwojtysiak/project/ExemploController.java
+++ project/src/main/java/br/pucrs/gwojtysiak/project/ExemploController.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -18,7 +22,9 @@ public class ExemploController {
     public ExemploController(){
         listaLivros = new ArrayList<>();
         listaLivros.add(new Livro(0, "Devil May Cry", "Hideki Kamiya", 2001));
+        listaLivros.add(new Livro(0, "Vergil's Inferno", "Hideki Kamiya", 2005));
         listaLivros.add(new Livro(1, "Guia do Mochileiro das Galáxias", "Douglas Adams", 1979));        
+        listaLivros.add(new Livro(2, "Guia do Mochileiro das Galáxias 2: O Inimigo Agora é Outro", "Douglas Adams", 2002));
     }
     @GetMapping("/")
     public String getMensagemInicial() {
@@ -51,4 +57,29 @@ public class ExemploController {
 
         return titulos;
     }
+
+    @GetMapping("/livrosautor")
+    public List<Livro> getLivroByAutor(@RequestParam(value = "autor") String autor) {
+        return listaLivros.stream()
+        .filter(livro -> livro.getAutor().equals(autor))
+        .toList();
+    }
+    
+    @GetMapping("/livrosautorano/{autor}/ano/{ano}")
+    public List<Livro> getLivroByAutorAno(@PathVariable(value="autor") String autor,
+                                @PathVariable(value="ano") int ano) {
+        return listaLivros.stream()
+        .filter(livro -> livro.getAutor().equals(autor))
+        .filter(livro -> livro.getAno() == ano)
+        .toList();
+    }
+
+    @PostMapping("/novolivro")
+    public String createNewBook(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+    
 }
